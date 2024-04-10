4289d61303df9821dd7ce31b20d396919531d8df
Brandon Castro <C1brandon626@gmail.com>
2024-04-10 14:59:42 -0700

    added meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 import ClientsPage from './pages/ClientPage';
 import Navbar from './components/NavBar';
 import Footer from './components/Footer';
-import './App.css'
-
-// import { SpeedInsights } from "@vercel/speed-insights/next"
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Navbar></Navbar>
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/clients" element={<ClientsPage />} />
       </Routes>
-      <Footer></Footer>
+      <Footer />
     </Router>
   );
 }

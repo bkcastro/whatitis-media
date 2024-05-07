038e78c0ad6a033ff0fb821633e55ac79b315aff
Brandon Castro <C1brandon626@gmail.com>
2024-05-07 15:21:15 -0700

    added clients back to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,13 +14,14 @@ function Navbar() {
                 <div className='h-min my-auto'>
                     <Link to="/" className='text-3xl sm:text-5xl md:text-6xl font-bold'>whatitis<span className='text-brandGreen'>.media</span></Link>
                     <p className='text-md sm:text-xl md:text-2xl lg:text-3xl text-center font-bold'>organic <span className='bg-gradient-to-r from-white to-brandGreen bg-clip-text text-transparent'>virality </span> <span className='text-brandGreen'>simplified</span></p>
-                    
+
                 </div>
             </div>
 
             <div className="text-brandPurple bg-gradient-to-r p-2 from-brandGreen to-brandDarkGreen w-screen flex flex-row justify-center gap-4">
                 <Link className='text-lg  font-bold hover:text-white' to="/">home</Link>
                 <Link className='text-lg font-bold hover:text-white' to="/about" >about</Link>
+                <Link className='text-lg font-bold hover:text-white' to="/clients" >clients</Link>
                 <a className='text-lg  font-bold hover:text-white' href='https://blairchap.substack.com/' target='_blank'>read</a>
                 <a className='text-lg  font-bold hover:text-white' to="/contact">book a call</a>
             </div>

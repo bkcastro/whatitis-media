6bf9553da73d6fc82d41e57b4c170b01224a3ee3
Brandon Castro <C1brandon626@gmail.com>
2024-05-07 15:20:18 -0700

    finished with quiz section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -11,7 +11,41 @@ import memesMatter from '../assets/creators/memes-matter.jpg';
 import gardenMarcus from '../assets/creators/garden-marcus.jpg';
 import recklessBen from '../assets/creators/recklessben.jpg';
 
+import creator1 from '../assets/Quiz_Faces/1.jpg';
+import creator2 from '../assets/Quiz_Faces/2.jpg';
+import creator3 from '../assets/Quiz_Faces/3.jpg';
+import creator4 from '../assets/Quiz_Faces/4.jpg';
+import creator5 from '../assets/Quiz_Faces/5.jpg';
+import creator6 from '../assets/Quiz_Faces/6.jpg';
+import creator7 from '../assets/Quiz_Faces/7.jpg';
+import creator8 from '../assets/Quiz_Faces/8.jpg';
+import creator9 from '../assets/Quiz_Faces/9.jpg';
+import creator10 from '../assets/Quiz_Faces/10.jpg';
+import creator11 from '../assets/Quiz_Faces/11.jpg';
+import creator12 from '../assets/Quiz_Faces/12.jpg';
+import creator13 from '../assets/Quiz_Faces/13.jpg';
+import creator14 from '../assets/Quiz_Faces/14.jpg';
+import creator15 from '../assets/Quiz_Faces/15.jpg';
+import creator16 from '../assets/Quiz_Faces/16.jpg';
+
+const ImageComponent = ({ src }) => (
+    <img src={src} alt="Creator" className='hover:opacity-70' />
+);
+
+
+
 function HomePage() {
+
+    const images = [
+        creator1, creator2, creator3, creator4, creator5, creator6,
+        creator7, creator8, creator9, creator10, creator11, creator12,
+        creator13, creator14, creator15, creator16
+    ];
+
+    const ImageComponent = ({ src }) => (
+        <img src={src} alt="Creator" className='hover:opacity-70' />
+    );
+
 
     const brands = [image1];
     const creators = [image2, memesMatter, gardenMarcus, recklessBen];
@@ -44,13 +78,13 @@ function HomePage() {
             </div> */}
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-2 h-auto bg-gradient-to-r from-brandGreen to-brandDarkGreen rounded-sm p-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 h-auto bg-gradient-to-r from-brandGreen to-brandDarkGreen rounded-sm p-5'>
             <div className='p-2 sm:p-3 md:p-4 flex justify-center items-center flex-col gap-5 sm:gap-6 md:gap-10'>
                 <p className='text-brandPurple text-3xl md:text-5xl text-center'>Try our quiz to see  what your creator archetype is?</p>
-                <a href={"https://blair-chapman.formaloo.co/d62tpo"} target='_blank'  className='p-6 w-fit text-brandGreen text-2xl sm:text rounded-full bg-gradient-to-r from-brandPurple to-brandLightPurple'>take the quiz</a>
+                <a href={"https://blair-chapman.formaloo.co/d62tpo"} target='_blank' className='p-4 sm:p-5 md:p-6 w-fit text-brandGreen text-2xl sm:text rounded-full bg-gradient-to-r from-brandPurple to-brandLightPurple hover:text-brandPurple'>take the quiz</a>
             </div>
-            <div className='bg-black w-full h-full'>
-
+            <div className='h-full grid grid-cols-4 grid-rows-4'>
+                {images.map(src => <ImageComponent key={src} src={src} />)}
             </div>
         </div>
 
@@ -58,7 +92,7 @@ function HomePage() {
         <Methodology></Methodology>
         {//<h1 className='text-center p-2'>Check out our <span className='text-brandGreen font-bold underline italic'>WORKSHEET</span> to see how your content matches up!</h1>
         }
-        
+
     </div>
 }
 
